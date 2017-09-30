ae62cff4fd7a590242c9e3e75f47905a446f77c8
Oliver Cordingley <>
2017-09-30 09:30:07 +0100

    made basic login/signup system

--- auth.php
+++ auth.php
@@ -16,7 +16,22 @@
       }
 
     }elseif (isset($_POST['signup'])) {
-      echo "Signing up...";
+      $fullname = mysqli_real_escape_string($conn,$_POST['fullname']);
+      $email = mysqli_real_escape_string($conn,$_POST['email']);
+      $username = mysqli_real_escape_string($conn,$_POST['username']);
+      $password = mysqli_real_escape_string($conn,$_POST['password']);
+
+      $hashedPassword = password_hash($password,PASSWORD_DEFAULT);
+
+      $sql = "INSERT INTO `users` (name, email, username, password)
+VALUES ('$fullname', '$email', '$username','$hashedPassword')";
+      $result = mysqli_query($conn,$sql);
+      if (!$result==1){
+        echo "There was an error signing you in. <a href='./index.html'>Go back!</a>";
+      } else {
+        $_SESSION['loggeduser'] = $username;
+        header("location:./dashboard/index.php");
+      }
     }
   } else {
     header("location:./index.html");
