0185cf04d5159201bc1d3cb247ebd52268654a5e
Oliver Cordingley <>
2017-09-30 10:47:44 +0100

    Fixed temporary rickroll

--- dashboard/index.php
+++ dashboard/index.php
@@ -10,6 +10,6 @@
   <body>
     yay it worked!!!!!
     <a href="../logout.php">Logout</a><br>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/DLzxrzFCyOs" frameborder="0" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/DLzxrzFCyOs?autoplay=1" frameborder="0" allowfullscreen></iframe>
   </body>
 </html>
