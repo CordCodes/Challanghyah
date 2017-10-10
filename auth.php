ddb6cf691cb9b8ea4a27bb3e5edab9ef728c21df
Oliver Cordingley <>
2017-10-10 18:36:47 +0100

    Made it so that you cannot make an account with the same uname/email

--- auth.php
+++ auth.php
@@ -13,6 +13,8 @@
       if ($realpassword){
         $_SESSION['loggeduser'] = row['username'];
         header("location:./dashboard/index.php");
+      } else{
+        eader('Location:./index.html?signuperror=signinerror');
       }
 
     }elseif (isset($_POST['signup'])) {
@@ -20,22 +22,37 @@
       $email = mysqli_real_escape_string($conn,$_POST['email']);
       $username = mysqli_real_escape_string($conn,$_POST['username']);
       $password = mysqli_real_escape_string($conn,$_POST['password']);
-
       $hashedPassword = password_hash($password,PASSWORD_DEFAULT);
 
-      $sql = "INSERT INTO `users` (name, email, username, password)
+      //Check to see if username or email already exists.
+      $testForUE = "SELECT * FROM `users` WHERE `username` = '$username' OR `email` = '$email'";
+      $result = mysqli_query($conn,$testForUE);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+      if (($row['email']==$email)&&($row['username']==$username)){
+        header('Location:./index.html?signuperror=eutaken');
+      }elseif ($row['email']==$email){
+        header('Location:./index.html?signuperror=emailtaken');
+      }elseif ($row['username']==$username){
+        header('Location:./index.html?signuperror=unametaken');
+      }else{
+        $sql = "INSERT INTO `users` (name, email, username, password)
 VALUES ('$fullname', '$email', '$username','$hashedPassword')";
-      $result = mysqli_query($conn,$sql);
-      if (!$result==1){
-        echo "There was an error signing you in. <a href='./index.html'>Go back!</a>";
-      } else {
-        $_SESSION['loggeduser'] = $username;
-        header("location:./dashboard/index.php");
+        $result = mysqli_query($conn,$sql);
+        if (!$result==1){
+          echo "There was an error signing you in. <a href='./index.html'>Go back!</a>";
+        } else {
+          $_SESSION['loggeduser'] = $username;
+          header("location:./dashboard/index.php");
+        }
       }
+
+
     }
+
   } else {
     header("location:./index.html");
   }
 
-  
+
 ?>
